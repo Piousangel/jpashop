a87f05983cdb2efedc44214916793483c5561acf
Piousangel <yyh7654@gmail.com>
2021-09-02 20:29:58 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+		//lombok -> 알아서 getter, setter 만들어준다
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+
+		System.out.println("data = " + data);
+
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+
+
+}
